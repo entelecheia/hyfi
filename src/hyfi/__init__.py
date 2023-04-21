b42300e9348d76cf977eb6c380e0ff0cc7401c0c
Young Joon Lee <entelecheia@hotmail.com>
2023-04-21 10:07:17 +0000

    fix(main): add aliases for HyFI class
    refactor(dependencies): update boto3, botocore, and setuptools versions

--- src/hyfi/__init__.py
+++ src/hyfi/__init__.py
@@ -3,4 +3,4 @@ from .cli import getLogger, hydra_main
 from .env import __about__ as about
 from .env import __global_config__ as global_config
 from .env import __hydra_version_base__
-from .main import HyFI
+from .main import HyFI, HyFI as H, HyFI as HI

d960a8f84c650ca0a701dc7464306031c8d70309
Young Joon Lee <entelecheia@hotmail.com>
2023-06-11 15:34:20 +0900

    feat(utils): add unittest for expand_posix_vars function

---
+++ tests/hyfi/utils/test_env.py
@@ -0,0 +1,28 @@
+import unittest
+from hyfi.utils.env import expand_posix_vars
+
+
+class TestExpandPosixVars(unittest.TestCase):
+    def test_expand_posix_vars(self):
+        # Test expanding $PWD and $USER variables
+        posix_expr = "The current working directory is $PWD and the user is $USER."
+        expanded_expr = expand_posix_vars(posix_expr)
+        self.assertIn("The current working directory is", expanded_expr)
+        self.assertIn("and the user is", expanded_expr)
+
+        # Test expanding custom variable
+        posix_expr = "The value of MY_VAR is $MY_VAR."
+        context = {"MY_VAR": "hello world"}
+        expanded_expr = expand_posix_vars(posix_expr, context)
+        self.assertIn("The value of MY_VAR is", expanded_expr)
+        self.assertIn("hello world", expanded_expr)
+
+        # Test expanding non-existent variable
+        posix_expr = "The value of NON_EXISTENT_VAR is $NON_EXISTENT_VAR."
+        expanded_expr = expand_posix_vars(posix_expr)
+        self.assertIn("The value of NON_EXISTENT_VAR is", expanded_expr)
+        self.assertIn("", expanded_expr)
+
+
+if __name__ == "__main__":
+    unittest.main()

048f49fc4d8a61d174a45697a96463b0d9afc037
Young Joon Lee <entelecheia@hotmail.com>
2023-06-14 11:38:22 +0900

    refactor(config): modify task path properties and use default project_data_root

--- src/hyfi/config/batch.py
+++ src/hyfi/config/batch.py
@@ -1,3 +1,4 @@
+import contextlib
 import random
 from pathlib import Path
 from typing import Any, Optional, Union
@@ -5,65 +6,97 @@ from typing import Any, Optional, Union
 from omegaconf import DictConfig
 from pydantic import BaseModel, validator
 
-from ..env import ProjectConfig, _to_config, _to_dict, getLogger
-from ..hydra import _compose, _load, _merge, _methods, _print, _save, _save_json
-from ..utils.lib import ensure_import_module
+from hyfi.env import ProjectConfig, _to_config, _to_dict, getLogger
+from hyfi.hydra import _compose, _load, _merge, _methods, _print, _save, _save_json
+from hyfi.utils.lib import ensure_import_module
 
 logger = getLogger(__name__)
 
 
 class PathConfig(BaseModel):
+    config_name: str = "__batch__"
+
+    project_data_root = ""
+    batch_name: str = "demo"
+    batch_output: str = ""
     task_name: str = "default-task"
     task_root: str = ""
-    batch_name: str = ""
+    task_outputs: str = ""
+    task_datasets: str = ""
+    task_library: str = ""
+    task_models: str = ""
+    task_cache: str = ""
+    task_tmp: str = ""
+    task_log: str = ""
 
     class Config:
         extra = "ignore"
 
-    def __init__(self, **data: Any):
+    def __init__(self, config_name: str = "__batch__", **data: Any):
         if not data:
-            data = _compose("path=__batch__")
-            logger.info(
-                "There are no arguments to initilize a config, using default config."
+            logger.debug(
+                "There are no arguments to initilize a config, using default config %s",
+                config_name,
             )
+            data = _compose(f"path={config_name}")
         super().__init__(**data)
 
     @property
+    def project_data_dir(self):
+        self.project_data_root = self.project_data_root or "./workspace"
+        return Path(self.project_data_root).absolute()
+
+    @property
     def root_dir(self):
+        self.task_root = (
+            self.task_root or (self.project_data_dir / self.task_name).as_posix()
+        )
         # return as an absolute path
         return Path(self.task_root).absolute()
 
     @property
     def output_dir(self):
-        return self.root_dir / "outputs"
+        self.task_outputs = self.task_outputs or (self.root_dir / "outputs").as_posix()
+        return Path(self.task_outputs).absolute()
 
     @property
     def batch_dir(self):
-        return self.output_dir / self.batch_name
+        self.batch_output = (
+            self.batch_output or (self.output_dir / self.batch_name).as_posix()
+        )
+        return Path(self.batch_output).absolute()
 
     @property
     def library_dir(self):
-        return self.root_dir / "libs"
+        self.task_library = self.task_library or (self.root_dir / "library").as_posix()
+        return Path(self.task_library).absolute()
 
     @property
-    def data_dir(self):
-        return self.root_dir / "data"
+    def dataset_dir(self):
+        self.task_datasets = (
+            self.task_datasets or (self.root_dir / "datasets").as_posix()
+        )
+        return Path(self.task_datasets).absolute()
 
     @property
     def model_dir(self):
-        return self.root_dir / "models"
+        self.task_models = self.task_models or (self.root_dir / "models").as_posix()
+        return Path(self.task_models).absolute()
 
     @property
     def cache_dir(self):
-        return self.root_dir / "cache"
+        self.task_cache = self.task_cache or (self.root_dir / "cache").as_posix()
+        return Path(self.task_cache).absolute()
 
     @property
     def tmp_dir(self):
-        return self.root_dir / "tmp"
+        self.task_tmp = self.task_tmp or (self.root_dir / "tmp").as_posix()
+        return Path(self.task_tmp).absolute()
 
     @property
     def log_dir(self):
-        log_dir = self.root_dir / "logs"
+        self.task_log = self.task_log or (self.root_dir / "logs").as_posix()
+        log_dir = Path(self.task_log).absolute()
         log_dir.mkdir(parents=True, exist_ok=True)
         return log_dir
 
@@ -316,12 +349,10 @@ class BaseConfigModel(BaseModel):
         if isinstance(objects, list):
             for obj in objects:
                 del obj
-        try:
-            from ..utils.gpu import GPUMon
+        with contextlib.suppress(ImportError):
+            from hyfi.utils.gpu import GPUMon
 
             GPUMon.release_gpu_memory()
-        except ImportError:
-            pass
 
 
 class BaseBatchModel(BaseConfigModel):
@@ -394,8 +425,8 @@ class BaseBatchModel(BaseConfigModel):
         return self.batch.batch_dir
 
     @property
-    def data_dir(self):
-        return self.path.data_dir
+    def dataset_dir(self):
+        return self.path.dataset_dir
 
     @property
     def verbose(self):

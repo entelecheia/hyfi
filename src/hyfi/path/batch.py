00384d0415287f776557fd8a8f77422f570e2fce
Young Joon Lee <entelecheia@hotmail.com>
2023-06-16 12:34:37 +0900

    refactor(path): split batch path config into its own class

--- src/hyfi/path/batch.py
+++ src/hyfi/path/batch.py
@@ -3,13 +3,13 @@ from typing import Any
 
 from pydantic import BaseModel
 
+from hyfi.hydra import _compose
 from hyfi.utils.logging import getLogger
-from hyfi.env import _compose
 
 logger = getLogger(__name__)
 
 
-class PathConfig(BaseModel):
+class BatchPathConfig(BaseModel):
     config_name: str = "__batch__"
 
     project_workspace_root = ""

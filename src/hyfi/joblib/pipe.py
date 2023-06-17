f39edc9fc1ca70563beb4411d8863006f4519dd1
Young Joon Lee <entelecheia@hotmail.com>
2023-06-17 15:39:22 +0900

    refactor(pipe): split pipe function into PIPE class
    refactor(apply): move apply function into PIPE class

--- src/hyfi/joblib/pipe.py
+++ src/hyfi/joblib/pipe.py
@@ -1,3 +1,6 @@
+"""
+A class to apply a pipe to a dataframe or a dictionary of dataframes.
+"""
 from tqdm.auto import tqdm
 
 from hyfi.hydra.main import SpecialKeys, _partial
@@ -8,58 +11,67 @@ from hyfi.utils.logging import getLogger
 logger = getLogger(__name__)
 
 
-def _pipe(data, pipe):
-    _func_ = pipe.get(SpecialKeys.FUNC)
-    _fn = _partial(_func_)
-    logger.info("Applying pipe: %s", _fn)
-    if isinstance(data, dict):
-        if "concat_dataframes" in str(_fn):
-            return _fn(data, pipe)
-        dfs = {}
-        for df_no, df_name in enumerate(data):
-            df_each = data[df_name]
+class PIPE:
+    """
+    A class to apply a pipe to a dataframe or a dictionary of dataframes.
+    """
 
-            logger.info(
-                "Applying pipe to dataframe [%s], %d/%d", df_name, df_no + 1, len(data)
-            )
+    @staticmethod
+    def pipe(data, pipe):
+        _func_ = pipe.get(SpecialKeys.FUNC)
+        _fn = _partial(_func_)
+        logger.info("Applying pipe: %s", _fn)
+        if isinstance(data, dict):
+            if "concat_dataframes" in str(_fn):
+                return _fn(data, pipe)
+            dfs = {}
+            for df_no, df_name in enumerate(data):
+                df_each = data[df_name]
 
-            pipe[SpecialKeys.SUFFIX.value] = df_name
-            dfs[df_name] = _fn(df_each, pipe)
-        return dfs
-    return _fn(data, pipe)
+                logger.info(
+                    "Applying pipe to dataframe [%s], %d/%d",
+                    df_name,
+                    df_no + 1,
+                    len(data),
+                )
 
+                pipe[SpecialKeys.SUFFIX.value] = df_name
+                dfs[df_name] = _fn(df_each, pipe)
+            return dfs
+        return _fn(data, pipe)
 
-def _apply(
-    func,
-    series,
-    description=None,
-    use_batcher=True,
-    minibatch_size=None,
-    num_workers=None,
-    **kwargs,
-):
-    batcher_instance = batcher.batcher_instance
-    if use_batcher and batcher_instance is not None:
-        batcher_minibatch_size = batcher_instance.minibatch_size
-        if minibatch_size is None:
-            minibatch_size = batcher_minibatch_size
-        if num_workers is not None:
-            batcher_instance.procs = int(num_workers)
-        if batcher_instance.procs > 1:
-            batcher_instance.minibatch_size = min(
-                int(len(series) / batcher_instance.procs) + 1, minibatch_size
-            )
-            logger.info(
-                f"Using batcher with minibatch size: {batcher_instance.minibatch_size}"
-            )
-            results = decorator_apply(func, batcher_instance, description=description)(
-                series
-            )
-            if batcher_instance is not None:
-                batcher_instance.minibatch_size = batcher_minibatch_size
-            return results
+    @staticmethod
+    def apply(
+        func,
+        series,
+        description=None,
+        use_batcher=True,
+        minibatch_size=None,
+        num_workers=None,
+        **kwargs,
+    ):
+        batcher_instance = batcher.batcher_instance
+        if use_batcher and batcher_instance is not None:
+            batcher_minibatch_size = batcher_instance.minibatch_size
+            if minibatch_size is None:
+                minibatch_size = batcher_minibatch_size
+            if num_workers is not None:
+                batcher_instance.procs = int(num_workers)
+            if batcher_instance.procs > 1:
+                batcher_instance.minibatch_size = min(
+                    int(len(series) / batcher_instance.procs) + 1, minibatch_size
+                )
+                logger.info(
+                    f"Using batcher with minibatch size: {batcher_instance.minibatch_size}"
+                )
+                results = decorator_apply(
+                    func, batcher_instance, description=description
+                )(series)
+                if batcher_instance is not None:
+                    batcher_instance.minibatch_size = batcher_minibatch_size
+                return results
 
-    if batcher_instance is None:
-        logger.info("Warning: batcher not initialized")
-    tqdm.pandas(desc=description)
-    return series.progress_apply(func)
+        if batcher_instance is None:
+            logger.info("Warning: batcher not initialized")
+        tqdm.pandas(desc=description)
+        return series.progress_apply(func)

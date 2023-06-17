9f7092b6916396118935ed45e1afc666b5f19aed
Young Joon Lee <entelecheia@hotmail.com>
2023-06-17 15:45:51 +0900

    refactor(joblib): use XC instead of _partial in pipe method

--- src/hyfi/joblib/pipe.py
+++ src/hyfi/joblib/pipe.py
@@ -3,7 +3,8 @@ A class to apply a pipe to a dataframe or a dictionary of dataframes.
 """
 from tqdm.auto import tqdm
 
-from hyfi.hydra.main import SpecialKeys, _partial
+from hyfi.hydra import SpecialKeys
+from hyfi.hydra.main import XC
 from hyfi.joblib.batch import batcher
 from hyfi.joblib.batch.apply import decorator_apply
 from hyfi.utils.logging import getLogger
@@ -19,7 +20,7 @@ class PIPE:
     @staticmethod
     def pipe(data, pipe):
         _func_ = pipe.get(SpecialKeys.FUNC)
-        _fn = _partial(_func_)
+        _fn = XC.partial(_func_)
         logger.info("Applying pipe: %s", _fn)
         if isinstance(data, dict):
             if "concat_dataframes" in str(_fn):

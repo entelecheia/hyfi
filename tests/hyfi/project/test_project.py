9cf8ece5ab40e5ea87a48a2be331c43b953e354f
Young Joon Lee <entelecheia@hotmail.com>
2023-06-18 21:19:17 +0900

    chore(project): comment out initialization step in test_project.py

--- tests/hyfi/project/test_project.py
+++ tests/hyfi/project/test_project.py
@@ -7,7 +7,7 @@ from pprint import pprint
 def test_project_config():
     os.environ["HYFI_PROJECT_NAME"] = "hyfi"
     config = ProjectConfig(project_name="test", project_root="./tmp", num_workers=2)
-    config.init_project()
+    # config.init_project()
     pprint(config.dict())
     assert config.project_name == "test"
     assert Path(config.project_root).absolute() == (Path.cwd() / "tmp").absolute()

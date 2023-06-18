1f40db360d6100591cf223fb30fc3c29ceadc96c
Young Joon Lee <entelecheia@hotmail.com>
2023-06-18 21:18:01 +0900

    refactor(path): update BatchPathConfig initialization method to allow for configuration of config group and call separate method to initialize configs

--- src/hyfi/path/batch.py
+++ src/hyfi/path/batch.py
@@ -16,19 +16,39 @@ class BatchPathConfig(TaskPathConfig):
     class Config:
         extra = "ignore"
 
-    def __init__(self, config_name: str = "__batch__", **data: Any):
+    def __init__(
+        self,
+        config_name: str = "__batch__",
+        config_group: str = "path",
+        **data: Any,
+    ):
         """
         Initialize the batch. This is the method you call when you want to initialize the batch from a config
 
         Args:
-                config_name: The name of the config you want to use
-                data: The data you want to initilize the
+            config_name: The name of the config you want to use
+            data: The data you want to initilize the
         """
         # Initialize the config with the given config_name.
-        data = Composer(
-            config_group=f"path={config_name}", config_data=data
-        ).config_as_dict
         super().__init__(**data)
+        self.initialize_configs(
+            config_name=config_name,
+            config_group=config_group,
+            **data,
+        )
+
+    def initialize_configs(
+        self,
+        config_name: str = "__batch__",
+        config_group: str = "path",
+        **data,
+    ):
+        # Initialize the config with the given config_name.
+        data = Composer(
+            config_group=f"{config_group}={config_name}",
+            config_data=data,
+        ).config_as_dict
+        self.__dict__.update(data)
 
     @property
     def batch_dir(self) -> Path:

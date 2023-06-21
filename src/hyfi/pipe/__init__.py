c2b222fcad0ab3028b137515a400d134e63c9a82
Young Joon Lee <entelecheia@hotmail.com>
2023-06-21 20:04:45 +0900

    refactor(pipe): update PipeConfig with new options

--- src/hyfi/pipe/__init__.py
+++ src/hyfi/pipe/__init__.py
@@ -4,7 +4,7 @@ A class to apply a pipe to a dataframe or a dictionary of dataframes.
 from typing import Any, Callable, Dict, List, Mapping, Optional, Sequence, Union
 
 import pandas as pd
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 from tqdm.auto import tqdm
 
 from hyfi.composer import SpecialKeys
@@ -19,15 +19,24 @@ logger = Logging.getLogger(__name__)
 class PipeConfig(BaseModel):
     """Pipe Configuration"""
 
-    _func_: Union[str, Dict] = "hyfi.pipe.funcs.data_class_methods"
+    _func_: Union[str, Dict] = "hyfi.pipe.funcs.apply_pipe_func"
+    _type_: str = "instance"
     _method_: str = ""
     apply_to: Union[str, List[str], None] = "text"
-    rcParams: dict = {}
+    rcParams: Union[Dict, None] = None
+    use_batcher: bool = True
+    num_workers: Optional[int] = 1
     verbose: bool = False
 
     class Config:
         arbitrary_types_allowed = True
         extra = "allow"
+
+    @validator("rcParams", pre=True, always=True)
+    def _validate_rcParams(cls, v):
+        if v is None:
+            return {}
+        return v
 
 
 class PIPE:

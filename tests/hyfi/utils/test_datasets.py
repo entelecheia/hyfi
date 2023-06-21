30b3cbbdef38ad6c0f42fec0b10e7d15db3ed73a
Young Joon Lee <entelecheia@hotmail.com>
2023-06-21 13:07:41 +0900

    refactor(utils): change name of dataframe loading test function, and add new dataset loading test function

--- tests/hyfi/utils/test_datasets.py
+++ tests/hyfi/utils/test_datasets.py
@@ -2,8 +2,8 @@ from hyfi.main import HyFI
 from pathlib import Path
 
 
-def test_dataframe_load_and_save():
-    """Test dataframe loading"""
+def test_dataframe_load_archive():
+    """Test dataframe loading from archive"""
     _path = HyFI.cached_path(
         "https://assets.entelecheia.ai/datasets/bok_minutes.zip",
         extract_archive=True,
@@ -16,7 +16,8 @@ def test_dataframe_load_and_save():
     # assert df.shape == (10, 2)
     HyFI.save_dataframes(df, "tmp/test.parquet")
 
-def test_dataframe_load_and_save2():
+
+def test_dataframe_load_and_save():
     """Test dataframe loading"""
     data_path = "https://assets.entelecheia.ai/datasets/bok_minutes/meta-bok_minutes-train.parquet"
     df = HyFI.load_data(data_files=data_path, use_cached=True, filetype="parquet")
@@ -27,5 +28,13 @@ def test_dataframe_load_and_save2():
     HyFI.save_dataframes(df, "tmp/meta-test.parquet")
 
 
+def test_dataset_load():
+    dataset = HyFI.load_dataset("glue", "mrpc")
+    print(dataset)
+    dataset = HyFI.load_data("glue", "mrpc", split=None)
+    print(dataset)
+
+
 if __name__ == "__main__":
-    test_dataframe_load_and_save2()
+    test_dataframe_load_and_save()
+    test_dataset_load()

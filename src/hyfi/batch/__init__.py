dc1ee7fd9eaee2367be683fd432c76ae03acd52b
Young Joon Lee <entelecheia@hotmail.com>
2023-06-25 10:33:16 +0900

    refactor(batch): update import statement for logger in __init__.py

--- src/hyfi/batch/__init__.py
+++ src/hyfi/batch/__init__.py
@@ -5,9 +5,9 @@ from typing import Any, Union
 from pydantic import validator
 
 from hyfi.composer import BaseConfig
-from hyfi.utils.logging import Logging
+from hyfi.utils.logging import LOGGING
 
-logger = Logging.getLogger(__name__)
+logger = LOGGING.getLogger(__name__)
 
 
 class BatchConfig(BaseConfig):

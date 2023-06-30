efbae91599506ffec13ac893452d7404889c1dfe
Young Joon Lee <entelecheia@hotmail.com>
2023-06-30 09:35:58 +0900

    feat(pipe): add general_instance_methods and general_external_funcs

--- src/hyfi/pipe/__init__.py
+++ src/hyfi/pipe/__init__.py
@@ -2,13 +2,48 @@
     Pipeline Functions
 """
 import pandas as pd
+from typing import Any
 
 from hyfi.joblib import BATCHER
-from hyfi.pipeline.configs import DataframePipeConfig
+from hyfi.pipeline.configs import DataframePipeConfig, PipeConfig
 from hyfi.utils.contexts import elapsed_timer
 from hyfi.utils.logging import LOGGING
 
 logger = LOGGING.getLogger(__name__)
+
+
+def general_instance_methods(obj: Any, config: PipeConfig):
+    with elapsed_timer(format_time=True) as elapsed:
+        obj = getattr(obj, config._run_)(**config.kwargs)
+
+        if config.verbose:
+            logger.info(" >> elapsed time: %s", elapsed())
+    return obj
+
+
+def general_external_funcs(obj: Any, config: PipeConfig):
+    _fn = config.get_run_func()
+    if _fn is None:
+        logger.warning("No function found for %s", config)
+        return obj
+    with elapsed_timer(format_time=True) as elapsed:
+        obj_arg = (
+            {config.pipe_obj_arg_name: obj}
+            if config.pipe_obj_arg_name and config.use_pipe_obj
+            else {}
+        )
+        obj_ = (
+            _fn(**obj_arg)
+            if obj_arg
+            else _fn(obj)
+            if obj is not None and config.use_pipe_obj
+            else _fn()
+        )
+        # return original data if no return value to continue pipeline
+        obj = obj if config.return_pipe_obj or obj_ is None else obj_
+        if config.verbose:
+            logger.info(" >> elapsed time: %s", elapsed())
+    return obj
 
 
 def dataframe_instance_methods(data: pd.DataFrame, config: DataframePipeConfig):

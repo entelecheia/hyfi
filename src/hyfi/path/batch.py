757dcc01b0c06477bd50c3a20afd2ade8455d00f
Young Joon Lee <entelecheia@hotmail.com>
2023-07-03 04:08:14 +0900

    refactor(path): improve batch directory handling

--- src/hyfi/path/batch.py
+++ src/hyfi/path/batch.py
@@ -10,24 +10,16 @@ class BatchPathConfig(TaskPathConfig):
     _config_name_: str = "__batch__"
     _config_group_: str = "path"
 
-    batch_outputs: str = ""
-
-    class Config:
-        extra = "ignore"
-
-    def initialize_configs(self, **config_kwargs):
-        super().initialize_configs(**config_kwargs)
+    batch_name: str = "demo-batch"
 
     @property
     def batch_dir(self) -> Path:
         """
         The directory where the batch is stored. It is used to determine where the results are stored for a batch of data to be processed.
 
-
         Returns:
                 The directory where the batch output is stored for a batch of data to be processed ( relative to the task output directory )
         """
-        self.batch_outputs = (
-            self.batch_outputs or (self.output_dir / "batch-outputs").as_posix()
-        )
-        return Path(self.batch_outputs).absolute()
+        path_ = self.output_dir / self.batch_name
+        path_.mkdir(parents=True, exist_ok=True)
+        return path_

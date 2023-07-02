2bcfec1297881f5432d6aa3ffc9e70c4af439784
Young Joon Lee <entelecheia@hotmail.com>
2023-07-03 04:09:19 +0900

    refactor(pipe): simplify config initialization
    This commit refactors the code in `src/hyfi/pipe/__init__.py` to simplify the config initialization process. The `DataframePipeConfig` object is now created using the `model_dump()` method instead of the `dict()` method. This change improves readability and ensures consistent behavior throughout the codebase.

--- src/hyfi/pipe/__init__.py
+++ src/hyfi/pipe/__init__.py
@@ -15,7 +15,7 @@ logger = LOGGING.getLogger(__name__)
 
 def general_instance_methods(obj: Any, config: PipeConfig):
     with elapsed_timer(format_time=True) as elapsed:
-        obj = getattr(obj, config._run_)(**config.kwargs)
+        obj = getattr(obj, config.run)(**config.kwargs)
 
         if config.verbose:
             logger.info(" >> elapsed time: %s", elapsed())
@@ -48,14 +48,14 @@ def general_external_funcs(obj: Any, config: PipeConfig):
 
 
 def dataframe_instance_methods(data: pd.DataFrame, config: DataframePipeConfig):
-    config = DataframePipeConfig(**config.dict())
+    config = DataframePipeConfig(**config.model_dump())
     with elapsed_timer(format_time=True) as elapsed:
         if config.columns:
             for col_name in config.columns:
                 logger.info("processing column: %s", col_name)
-                data[col_name] = getattr(data[col_name], config._run_)(**config.kwargs)
+                data[col_name] = getattr(data[col_name], config.run)(**config.kwargs)
         else:
-            data = getattr(data, config._run_)(**config.kwargs)
+            data = getattr(data, config.run)(**config.kwargs)
 
         if config.verbose:
             logger.info(" >> elapsed time: %s", elapsed())
@@ -64,7 +64,7 @@ def dataframe_instance_methods(data: pd.DataFrame, config: DataframePipeConfig):
 
 
 def dataframe_external_funcs(data: pd.DataFrame, config: DataframePipeConfig):
-    config = DataframePipeConfig(**config.dict())
+    config = DataframePipeConfig(**config.model_dump())
     _fn = config.get_run_func()
     if _fn is None:
         logger.warning("No function found for %s", config)

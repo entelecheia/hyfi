0f56cbadd1f8d8169362094dc5cff0e79ac9866a
Young Joon Lee <entelecheia@hotmail.com>
2023-07-03 04:06:21 +0900

    refactor(copier): update Copier class configuration options
    The Copier class in src/hyfi/copier/__init__.py has been updated to include new configuration options. Specifically, the 'filetypes' and 'exclude' attributes have been changed to accept either a list or a string as input. This allows for greater flexibility in specifying filetypes and exclusion patterns.

--- src/hyfi/copier/__init__.py
+++ src/hyfi/copier/__init__.py
@@ -26,7 +26,7 @@ import os
 from dataclasses import field
 from pathlib import Path
 from shutil import copy2, rmtree
-from typing import List
+from typing import List, Optional, Union
 
 from pathspec import PathSpec
 from pydantic.dataclasses import dataclass
@@ -77,8 +77,8 @@ class Copier:
 
     src_path: Path = field(default=Path("conf"))
     dst_path: Path = field(default=Path("."))
-    filetypes: List = field(default_factory=list)
-    exclude: List = field(default_factory=list)
+    filetypes: Optional[Union[List, str]] = field(default=None)
+    exclude: Optional[Union[List, str]] = field(default=None)
     skip_if_exists: bool = False
     cleanup_on_error: bool = True
     overwrite: bool = False

2a5156ac2f3840667e989ae741f9922b0b28b175
Young Joon Lee <entelecheia@hotmail.com>
2023-07-04 07:14:31 +0900

    refactor(about): improve documentation
    This commit improves the documentation in the `AboutConfig` class of the `about` module. It adds docstrings to the class and its attributes, providing clear explanations of their purpose and usage.

--- src/hyfi/about/__init__.py
+++ src/hyfi/about/__init__.py
@@ -1,3 +1,10 @@
+"""This module contains the About Configuration for the HyFI package.
+
+It defines the AboutConfig class, which is a Pydantic BaseModel that contains
+metadata about the package, such as its name, version, authors, and license.
+It also defines the model_config attribute, which is a ConfigDict that allows
+extra configuration options to be added to the AboutConfig instance.
+"""
 from pydantic import BaseModel, ConfigDict
 
 from hyfi.utils.logging import LOGGING
@@ -6,7 +13,19 @@ logger = LOGGING.getLogger(__name__)
 
 
 class AboutConfig(BaseModel):
-    """About Configuration"""
+    """A Pydantic BaseModel that contains metadata about the package.
+
+    Attributes:
+        __package_name__ (str): The name of the package.
+        name (str): The display name of the package.
+        authors (str): The author(s) of the package.
+        description (str): A brief description of the package.
+        homepage (str): The URL of the package's homepage.
+        license (str): The license under which the package is distributed.
+        version (str): The version number of the package.
+        model_config (ConfigDict): A ConfigDict that allows extra configuration
+            options to be added to the AboutConfig instance.
+    """
 
     __package_name__: str = "hyfi"
     name: str = "HyFI"
@@ -22,8 +41,10 @@ class AboutConfig(BaseModel):
 
     @property
     def config_module(self) -> str:
+        """Returns the name of the configuration module."""
         return f"{self.__package_name__}.conf"
 
     @property
     def config_path(self) -> str:
+        """Returns the path to the configuration module."""
         return f"pkg://{self.config_module}"

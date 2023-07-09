32f01e8a91e00446f9048d3658be8f09016e49f3
Young Joon Lee <entelecheia@hotmail.com>
2023-07-09 14:29:46 +0900

    refactor(composer): refactor `partial` and `instantiate` methods

--- src/hyfi/composer/__init__.py
+++ src/hyfi/composer/__init__.py
@@ -7,7 +7,7 @@ import os
 import re
 from enum import Enum
 from pathlib import Path
-from typing import IO, Any, Dict, List, Mapping, Optional, Set, Tuple, Union
+from typing import IO, Any, Callable, Dict, List, Mapping, Optional, Set, Tuple, Union
 
 import hydra
 from omegaconf import DictConfig, ListConfig, OmegaConf, SCMode
@@ -20,6 +20,7 @@ from hyfi.__global__ import (
     __hydra_version_base__,
 )
 from hyfi.utils.logging import LOGGING
+from hyfi.utils.packages import PKGs
 
 if level := os.environ.get("HYFI_LOG_LEVEL"):
     LOGGING.setLogger(level)
@@ -769,6 +770,125 @@ class Composer(BaseModel):
         key_ = re.sub(r"^_pipe_$", "pipe_target", key_)
         key_ = re.sub(r"^_run_$", "run", key_)
         return key_
+
+    @staticmethod
+    def partial(
+        config: Union[str, Dict],
+        *args: Any,
+        **kwargs: Any,
+    ) -> Callable:
+        """
+        Returns a callable object that is a partial version of the function or class specified in the config object.
+
+        Args:
+            config: An config object describing what to call and what params to use.
+                    In addition to the parameters, the config must contain:
+                    _target_ : target class or callable name (str)
+                    And may contain:
+                    _partial_: If True, return functools.partial wrapped method or object
+                                False by default. Configure per target.
+            args: Optional positional parameters pass-through
+            kwargs: Optional named parameters to override
+                    parameters in the config object. Parameters not present
+                    in the config objects are being passed as is to the target.
+                    IMPORTANT: dataclasses instances in kwargs are interpreted as config
+                                and cannot be used as passthrough
+        Returns:
+            A callable object that is a partial version of the function or class specified in the config object.
+        """
+        if isinstance(config, str):
+            config = {SpecialKeys.TARGET.value: config}
+        else:
+            config = Composer.to_dict(config)
+        if not isinstance(config, dict):
+            raise ValueError("config must be a dict or a str")
+        config[SpecialKeys.PARTIAL.value] = True
+        rc_kwargs_ = config.pop(SpecialKeys.KWARGS, {})
+        if rc_kwargs_ and kwargs:
+            kwargs.update(rc_kwargs_)
+        return Composer.instantiate(config, *args, **kwargs)
+
+    @staticmethod
+    def instantiate(config: Any, *args: Any, **kwargs: Any) -> Any:
+        """
+        Instantiates an object using the provided config object.
+
+        Args:
+            config: An config object describing what to call and what params to use.
+                    In addition to the parameters, the config must contain:
+                    _target_ : target class or callable name (str)
+                    And may contain:
+                    _args_: List-like of positional arguments to pass to the target
+                    _recursive_: Construct nested objects as well (bool).
+                                    False by default.
+                                    may be overridden via a _recursive_ key in
+                                    the kwargs
+                    _convert_: Conversion strategy
+                            none    : Passed objects are DictConfig and ListConfig, default
+                            partial : Passed objects are converted to dict and list, with
+                                    the exception of Structured Configs (and their fields).
+                            all     : Passed objects are dicts, lists and primitives without
+                                    a trace of OmegaConf containers
+                    _partial_: If True, return functools.partial wrapped method or object
+                                False by default. Configure per target.
+                    _args_: List-like of positional arguments
+            args: Optional positional parameters pass-through
+            kwargs: Optional named parameters to override
+                    parameters in the config object. Parameters not present
+                    in the config objects are being passed as is to the target.
+                    IMPORTANT: dataclasses instances in kwargs are interpreted as config
+                                and cannot be used as passthrough
+
+        Returns:
+            if _target_ is a class name: the instantiated object
+            if _target_ is a callable: the return value of the call
+        """
+        verbose = config.get("verbose", False)
+        if not Composer.is_instantiatable(config):
+            if verbose:
+                logger.info("Config is not instantiatable, returning config")
+            return config
+        _recursive_ = config.get(SpecialKeys.RECURSIVE, False)
+        if SpecialKeys.RECURSIVE not in kwargs:
+            kwargs[SpecialKeys.RECURSIVE.value] = _recursive_
+        if verbose:
+            logger.info("instantiating %s ...", config.get(SpecialKeys.TARGET))
+        return hydra.utils.instantiate(config, *args, **kwargs)
+
+    @staticmethod
+    def getsource(obj: Any) -> str:
+        """
+        Return the source code of the object.
+
+        Args:
+            obj: The object to get the source code of.
+
+        Returns:
+            The source code of the object as a string.
+
+        """
+        try:
+            target_string = ""
+            if Composer.is_config(obj):
+                if SpecialKeys.TARGET in obj:
+                    target_string = obj[SpecialKeys.TARGET]
+            elif isinstance(obj, str):
+                target_string = obj
+            return PKGs.getsource(target_string) if target_string else ""
+        except Exception as e:
+            logger.error(f"Error getting source: {e}")
+            return ""
+
+    @staticmethod
+    def viewsource(obj: Any):
+        """
+        Print the source code of the object.
+
+        Args:
+            obj: The object to print the source code of.
+
+        """
+        print(Composer.getsource(obj))
 
 
 class BaseConfig(BaseModel):

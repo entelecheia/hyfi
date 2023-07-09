69cc2f6ae33c9b7351ef77c3b4033a1f0fad09ea
Young Joon Lee <entelecheia@hotmail.com>
2023-07-10 07:31:45 +0900

    chore(scripts): add generate_config_docs.py to create config markdown documentation from yaml files

---
+++ scripts/generate_config_docs.py
@@ -0,0 +1,57 @@
+"""
+Interate folders unlder src/hyfi/conf and generate config markdown docs named as the folder name.
+
+Contents of the markdown file are yaml files in the folder.
+
+Example of the generated markdown file:
+
+```markdown
+# Config group name (folder name)
+
+Config location: `conf/<config_group_name>`
+
+## `file_name.yaml`
+
+```yaml
+{% include "../../src/hyfi/conf/<config_group_name>/file_name.yaml" %}
+```
+
+```
+
+Variables:
+
+config_folder = "src/hyfi/conf"
+output_docs_folder = "docs/configs"
+
+"""
+import os
+import yaml
+from jinja2 import Template
+
+config_folder = "src/hyfi/conf"
+output_docs_folder = "docs/configs"
+
+if not os.path.exists(output_docs_folder):
+    os.makedirs(output_docs_folder)
+
+for folder in os.listdir(config_folder):
+    folder_path = os.path.join(config_folder, folder)
+    if os.path.isdir(folder_path):
+        output_file = os.path.join(output_docs_folder, f"{folder}.md")
+        with open(output_file, "w") as f:
+            f.write(f"# {folder}\n\n")
+            f.write(f"Config location: `conf/{folder}`\n\n")
+            for file in os.listdir(folder_path):
+                if file.endswith(".yaml"):
+                    file_path = os.path.join(folder_path, file)
+                    with open(file_path) as yaml_file:
+                        yaml_data = yaml.safe_load(yaml_file)
+                        template = Template("```yaml\n{{ data }}\n```")
+                        f.write(f"## `{file}`\n\n")
+                        f.write("```yaml\n")
+                        f.write(
+                            "{% include '../../src/hyfi/conf/"
+                            + f"{folder}/{file}'"
+                            + " %}"
+                        )
+                        f.write("\n```\n\n")

d6bbdfa7554177ef0668eb26d1dc1e87671679ae
Young Joon Lee <entelecheia@hotmail.com>
2023-07-09 19:03:35 +0900

    refactor(utils): update gpumon

--- src/hyfi/utils/gpumon.py
+++ src/hyfi/utils/gpumon.py
@@ -72,9 +72,13 @@ class GPUMon(Thread):
         interval=900,
         verbose=False,
         includeNan=False,
-        excludeID=[],
-        excludeUUID=[],
+        excludeID=None,
+        excludeUUID=None,
     ):
+        if excludeID is None:
+            excludeID = []
+        if excludeUUID is None:
+            excludeUUID = []
         return GPUtil.getFirstAvailable(
             order=order,
             maxLoad=maxLoad,
@@ -140,3 +144,37 @@ def set_cuda(device=0):
     except ImportError as e:
         os.environ["CUDA_VISIBLE_DEVICES"] = ""
         raise CudaDeviceNotFoundError("Cuda device not found") from e
+
+
+class GPUs:
+    ###############################
+    # GPU Utility functions
+    ###############################
+    @staticmethod
+    def nvidia_smi():
+        return nvidia_smi()
+
+    @staticmethod
+    def set_cuda(device=0):
+        return set_cuda(device)
+
+    @staticmethod
+    def gpu_usage(all=False, attrList=None, useOldCode=False):
+        """
+        Show GPU utilization in human readable format. This is a wrapper around the GPUtil library.
+
+        Args:
+                all: If True show all available GPUs ( default : False )
+                attrList: List of attributes to show ( default : None )
+                useOldCode: If True use old code instead of new code ( default : False )
+
+        Returns:
+                A string with the
+        """
+        try:
+            from GPUtil import showUtilization  # type: ignore
+        except ImportError:
+            logger.error("GPUtil is not installed. To install, run: pip install GPUtil")
+            return
+
+        return showUtilization(all, attrList, useOldCode)

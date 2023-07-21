a438adf577b10a8396c7542873a1a686cd67b2b4
Young Joon Lee <entelecheia@hotmail.com>
2023-07-22 07:00:30 +0900

    refactor(test_composer): use HyFI.get_caller_module_name instead of inspect module

--- tests/hyfi/composer/test_composer.py
+++ tests/hyfi/composer/test_composer.py
@@ -1,19 +1,17 @@
 from hyfi.composer import Composer
-
-import inspect
+from hyfi.main import HyFI
 
 
 def my_function():
-    caller_module = inspect.getmodule(inspect.stack()[1][0])
-    caller_module_name = caller_module.__name__
+    caller_module_name = HyFI.get_caller_module_name()
     print(f"The name of the caller module is {caller_module_name}")
 
 
 def test_composer():
+    my_function()
     cfg = Composer._compose("about", config_module="hyfi.conf")
     print(cfg)
 
 
 if __name__ == "__main__":
-    my_function()
     test_composer()

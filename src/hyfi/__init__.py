d9beab0f9b6b02bb7e774e20fda5b90ee1a9f163
Young Joon Lee <entelecheia@hotmail.com>
2023-07-23 06:07:05 +0900

    fix(hyfi): replace about with global_hyfi in module imports

--- src/hyfi/__init__.py
+++ src/hyfi/__init__.py
@@ -8,7 +8,7 @@
     Other module folders are plural, e.g. `utils` instead of `util`.
 """
 from hyfi.__cli__ import hydra_main, hyfi_main
-from hyfi.core import __about__ as about
+from hyfi.core import __global_hyfi__ as global_hyfi
 from hyfi.core import __hydra_version_base__
 from hyfi.core.config import __global_config__ as global_config
 from hyfi.main import HyFI
@@ -17,8 +17,8 @@ from hyfi.main import HyFI as HI
 from hyfi.utils.logging import LOGGING
 
 __all__ = [
-    "about",
     "global_config",
+    "global_hyfi",
     "hydra_main",
     "hyfi_main",
     "HyFI",

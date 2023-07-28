9887ee1eca9d92bb689d1e015361f874fb4f1391
Young Joon Lee <entelecheia@hotmail.com>
2023-07-28 13:55:55 +0900

    feat(tests): add new test_base.py for hyfi composer

---
+++ tests/hyfi/composer/test_base.py
@@ -0,0 +1,10 @@
+from hyfi.composer.base import TestModel
+
+
+def test_base_model():
+    cfg = TestModel.save_hyfi_config(config_root="config")
+    print(cfg)
+
+
+if __name__ == "__main__":
+    test_base_model()

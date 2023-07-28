3c20d05aad9a10461f8a6157ebf37575551e2c99
Young Joon Lee <entelecheia@hotmail.com>
2023-07-28 14:41:41 +0900

    refactor(test_generator): change HyFI.save_hyfi_config to HyFI.save_hyfi_pipe_config and simplify print statements

--- tests/hyfi/composer/test_generator.py
+++ tests/hyfi/composer/test_generator.py
@@ -7,12 +7,12 @@ from hyfi.pipe.datasets import (
 
 
 def test_generate_config():
-    cfg = HyFI.save_hyfi_config(sample_dataset)
-    print(HyFI.to_yaml(cfg))
-    cfg = HyFI.save_hyfi_config(save_dataset_to_disk)
-    print(HyFI.to_yaml(cfg))
-    cfg = HyFI.save_hyfi_config(load_dataset_from_disk)
-    print(HyFI.to_yaml(cfg))
+    cfg = HyFI.save_hyfi_pipe_config(sample_dataset)
+    print(cfg)
+    cfg = HyFI.save_hyfi_pipe_config(save_dataset_to_disk)
+    print(cfg)
+    cfg = HyFI.save_hyfi_pipe_config(load_dataset_from_disk, use_pipe_obj=False)
+    print(cfg)
 
 
 if __name__ == "__main__":

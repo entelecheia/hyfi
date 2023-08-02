cf46d6f825d4d8fb859454f3d04907b6c0925a77
Young Joon Lee <entelecheia@hotmail.com>
2023-08-02 10:36:10 +0900

    refactor(test_generator): replace save_hyfi_pipe_config with generate_pipe_config

--- tests/hyfi/composer/test_generator.py
+++ tests/hyfi/composer/test_generator.py
@@ -1,35 +1,40 @@
 from hyfi import HyFI
 from hyfi.composer import PipeTargetTypes
+from hyfi.composer import BaseModel
+
+
+def test_generate_config_for_composers():
+    BaseModel.generate_config()
 
 
 def test_generate_config():
-    cfg = HyFI.save_hyfi_pipe_config(HyFI.sample_dataset)
+    cfg = HyFI.generate_pipe_config(HyFI.sample_dataset)
     print(cfg)
-    cfg = HyFI.save_hyfi_pipe_config(HyFI.save_dataset_to_disk)
+    cfg = HyFI.generate_pipe_config(HyFI.save_dataset_to_disk)
     print(cfg)
-    cfg = HyFI.save_hyfi_pipe_config(HyFI.load_dataset_from_disk, use_pipe_obj=False)
+    cfg = HyFI.generate_pipe_config(HyFI.load_dataset_from_disk, use_pipe_obj=False)
     print(cfg)
 
 
 def test_generate_config_for_HyFI():
-    cfg = HyFI.save_hyfi_pipe_config(
+    cfg = HyFI.generate_pipe_config(
         HyFI.load_dataframes,
         pipe_target_type=PipeTargetTypes.DATAFRAME_EXTERNAL_FUNCS,
         use_pipe_obj=False,
     )
     print(cfg)
-    cfg = HyFI.save_hyfi_pipe_config(
+    cfg = HyFI.generate_pipe_config(
         HyFI.save_dataframes,
         pipe_target_type=PipeTargetTypes.DATAFRAME_EXTERNAL_FUNCS,
         use_pipe_obj=True,
     )
     print(cfg)
-    cfg = HyFI.save_hyfi_pipe_config(
+    cfg = HyFI.generate_pipe_config(
         HyFI.load_dataset,
         use_pipe_obj=False,
     )
     print(cfg)
-    cfg = HyFI.save_hyfi_pipe_config(
+    cfg = HyFI.generate_pipe_config(
         HyFI.load_data,
         use_pipe_obj=False,
     )
@@ -37,5 +42,6 @@ def test_generate_config_for_HyFI():
 
 
 if __name__ == "__main__":
+    test_generate_config_for_composers()
     test_generate_config()
     test_generate_config_for_HyFI()

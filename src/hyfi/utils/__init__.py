1c2170db1a577b4770026ed351a332c231f71ec8
Young Joon Lee <entelecheia@hotmail.com>
2023-08-02 13:57:35 +0900

    chore(utils): add multiple utility modules

---
+++ src/hyfi/utils/__init__.py
@@ -0,0 +1,21 @@
+from .conf import CONFs
+from .datasets import DATASETs
+from .envs import ENVs
+from .funcs import FUNCs
+from .gpumon import GPUs
+from .iolibs import IOLIBs
+from .logging import LOGGING
+from .notebooks import NBs
+from .packages import PKGs
+
+__all__ = [
+    "CONFs",
+    "DATASETs",
+    "ENVs",
+    "FUNCs",
+    "GPUs",
+    "IOLIBs",
+    "LOGGING",
+    "NBs",
+    "PKGs",
+]

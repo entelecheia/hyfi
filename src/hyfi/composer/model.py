90cc87477cce7bc36f9625afc161ca6a53d9424a
Young Joon Lee <entelecheia@hotmail.com>
2023-08-02 10:34:51 +0900

    feat(model): add auto-populate, auto-generate, exclude attributes and related functionality, update config group default values and related changes in compose mechanism

--- src/hyfi/composer/model.py
+++ src/hyfi/composer/model.py
@@ -2,7 +2,7 @@
     HiFY Base Model Class
 """
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Set
 
 from pydantic import BaseModel as PydanticBaseModel
 from pydantic import ConfigDict, model_validator
@@ -21,10 +21,20 @@ __all__ = ["BaseModel", "model_validator"]
 class BaseModel(PydanticBaseModel):
     """
     Base class for all Pydantic models.
+
+    Attributes:
+        _config_name_ (str): The name of the model.
+        _config_group_ (str): The group of the model.
+        _auto_populate_ (bool): Whether to auto-populate the model with defaults from the config.
+        _auto_generate_ (bool): Whether to auto-generate the config for the model.
+
     """
 
     _config_name_: str = "__init__"
-    _config_group_: str = ""
+    _config_group_: str = "/composer"
+    _auto_populate_: bool = False
+    _auto_generate_: bool = False
+    _exclude_: Set[str] = set()
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
@@ -32,8 +42,70 @@ class BaseModel(PydanticBaseModel):
         validate_assignment=False,
     )  # type: ignore
 
+    @model_validator(mode="before")
+    def validate_model_config_before(cls, data):
+        # logger.debug("Validating model config before validating each field.")
+        _auto_populate_ = data.get("_auto_populate_", getattr(cls._auto_populate_, "default", False))  # type: ignore
+        if not _auto_populate_:
+            logger.debug("Auto-populate is disabled.")
+            return data
+        _config_name_ = data.get("_config_name_", getattr(cls._config_name_, "default", "__init__"))  # type: ignore
+        _config_group_ = data.get("_config_group_", getattr(cls._config_group_, "default"))  # type: ignore
+        _class_name_ = cls.__name__  # type: ignore
+        if not _config_group_:
+            logger.debug("There is no config group specified.")
+            return data
+        # Initialize the config with the given config_name.
+        logger.info(
+            "Composing `%s` class with `%s` config in `%s` group.",
+            _class_name_,
+            _config_name_,
+            _config_group_,
+        )
+        config_group = f"{_config_group_}={_config_name_}"
+        cfg = Composer(
+            config_group=config_group,
+            config_data=data,
+            throw_on_compose_failure=False,
+        ).config_as_dict
+        data = Composer.update(cfg, data)
+        # Exclude any attributes specified in the class's `exclude` list.
+        exclude = getattr(cls._exclude_, "default", set())  # type: ignore
+        for name in exclude:
+            if name in data:
+                del data[name]  # type: ignore
+        return data
+
+    @property
+    def config_name(self) -> str:
+        """
+        Returns the name of the model.
+        """
+        return self._config_name_
+
+    @property
+    def config_group(self) -> str:
+        """
+        Returns the group of the model.
+        """
+        return self._config_group_
+
+    @property
+    def auto_populate(self) -> bool:
+        """
+        Returns whether the model should be auto-populated.
+        """
+        return self._auto_populate_
+
+    @property
+    def auto_generate(self) -> bool:
+        """
+        Returns whether the model should be auto-generated.
+        """
+        return self._auto_generate_
+
     @classmethod
-    def save_hyfi_config(
+    def generate_config(
         cls,
         config_name: Optional[str] = None,
         config_path: str = None,
@@ -69,7 +141,10 @@ class BaseModel(PydanticBaseModel):
         ) or cls._config_name_
         filename = f"{config_name}.yaml"
         config_root = config_root or global_hyfi.config_root
-        config_path = config_path or getattr(cls._config_group_, "default") or "model"
+        _config_group_ = getattr(cls._config_group_, "default")
+        if _config_group_ and _config_group_.startswith("/"):
+            _config_group = _config_group_[1:]
+        config_path = config_path or _config_group or "test"
         config_path = Path(config_root) / config_path
         config_path.mkdir(parents=True, exist_ok=True)
         config_path /= filename
@@ -81,14 +156,14 @@ class BaseModel(PydanticBaseModel):
 
 class InnerTestModel(BaseModel):
     _config_name_: str = "__inner__"
-    _config_group_: str = "test"
+    _config_group_: str = "/test"
 
     name: str = "inner"
 
 
 class TestModel(BaseModel):
     _config_name_: str = "__test__"
-    _config_group_: str = "test"
+    _config_group_: str = "/test"
     inner: InnerTestModel = InnerTestModel()
 
     name: str = "test"
@@ -112,9 +187,9 @@ def sanitized_config(
     for key, value in config.items():
         if hasattr(value, "_config_group_") and hasattr(value, "_config_name_"):
             if value._config_group_ == key:
-                defaults.append({f"/{value._config_group_}": value._config_name_})
+                defaults.append({f"{value._config_group_}": value._config_name_})
             else:
-                defaults.append({f"/{value._config_group_}@{key}": value._config_name_})
+                defaults.append({f"{value._config_group_}@{key}": value._config_name_})
         else:
             value = sanitized_default_value(value)
             _config[key] = value

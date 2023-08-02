40179729924be364dce8cf20e7de4027491b57cc
Young Joon Lee <entelecheia@hotmail.com>
2023-08-02 15:11:51 +0900

    refactor(pipeline): move run_pipeline and run_pipe logic to task object

--- src/hyfi/pipeline/pipeline.py
+++ src/hyfi/pipeline/pipeline.py
@@ -1,22 +1,15 @@
 """
 A class to run a pipeline.
 """
-from functools import reduce
 from typing import Any, Dict, Optional, Union
 
-from hyfi.composer import Composer
 from hyfi.project import ProjectConfig
 from hyfi.task import TaskConfig
-from hyfi.utils.contexts import change_directory, elapsed_timer
+from hyfi.utils.contexts import elapsed_timer
 from hyfi.utils.logging import LOGGING
 from hyfi.workflow import WorkflowConfig
 
-from .config import (
-    PipeConfig,
-    PipelineConfig,
-    Pipelines,
-    get_running_configs,
-)
+from .config import PipeConfig, PipelineConfig, Pipelines, get_running_configs, run_pipe
 
 logger = LOGGING.getLogger(__name__)
 
@@ -43,37 +36,38 @@ class PIPELINEs:
         Returns:
             The result of the pipeline
         """
-        # If config is not a PipelineConfig object it will be converted to a PipelineConfig object.
-        if not isinstance(config, PipelineConfig):
-            config = PipelineConfig(**Composer.to_dict(config))
-        pipes = config.get_pipes()
-        if (
-            initial_object is None
-            and config.initial_object is not None
-            and Composer.is_instantiatable(config.initial_object)
-        ):
-            initial_object = Composer.instantiate(config.initial_object)
-        # Return initial object for the initial object
-        if not pipes:
-            logger.warning("No pipes specified")
-            return initial_object
-
-        pipe_names = [pipe.run for pipe in pipes]
-        logger.info("Applying %s pipes: %s", len(pipe_names), pipe_names)
-        # Run the task in the current directory.
         if task is None:
             task = TaskConfig()
-        with elapsed_timer(format_time=True) as elapsed:
-            with change_directory(task.workspace_dir):
-                rst = reduce(PIPELINEs.run_pipe, pipes, initial_object)
-            # Print the elapsed time.
-            if config.verbose:
-                logger.info(
-                    " >> elapsed time for the pipeline with %s pipes: %s",
-                    len(pipes),
-                    elapsed(),
-                )
-        return rst
+        return task.run_pipeline(config, initial_object)
+        # # If config is not a PipelineConfig object it will be converted to a PipelineConfig object.
+        # if not isinstance(config, PipelineConfig):
+        #     config = PipelineConfig(**Composer.to_dict(config))
+        # pipes = config.get_pipes()
+        # if (
+        #     initial_object is None
+        #     and config.initial_object is not None
+        #     and Composer.is_instantiatable(config.initial_object)
+        # ):
+        #     initial_object = Composer.instantiate(config.initial_object)
+        # # Return initial object for the initial object
+        # if not pipes:
+        #     logger.warning("No pipes specified")
+        #     return initial_object
+
+        # pipe_names = [pipe.run for pipe in pipes]
+        # logger.info("Applying %s pipes: %s", len(pipe_names), pipe_names)
+        # # Run the task in the current directory.
+        # with elapsed_timer(format_time=True) as elapsed:
+        #     with change_directory(task.workspace_dir):
+        #         rst = reduce(PIPELINEs.run_pipe, pipes, initial_object)
+        #     # Print the elapsed time.
+        #     if config.verbose:
+        #         logger.info(
+        #             " >> elapsed time for the pipeline with %s pipes: %s",
+        #             len(pipes),
+        #             elapsed(),
+        #         )
+        # return rst
 
     @staticmethod
     def run_pipe(
@@ -90,37 +84,39 @@ class PIPELINEs:
         Returns:
             The result of the pipe
         """
-        # Create a PipeConfig object if not already a PipeConfig.
-        if not isinstance(config, PipeConfig):
-            config = PipeConfig(**Composer.to_dict(config))
-        pipe_fn = config.get_pipe_func()
-        # Return the object that is being used to execute the pipe function.
-        if pipe_fn is None:
-            logger.warning("No pipe function specified")
-            return obj
-        # Run a pipe with the pipe_fn
-        if config.verbose:
-            logger.info("Running a pipe with %s", config.pipe_target)
-        # Apply pipe function to each object.
-        if isinstance(obj, dict):
-            objs = {}
-            # Apply pipe to each object.
-            for no, name in enumerate(obj):
-                obj_ = obj[name]
+        return run_pipe(obj, config)
 
-                # Apply pipe to an object.
-                if config.verbose:
-                    logger.info(
-                        "Applying pipe to an object [%s], %d/%d",
-                        name,
-                        no + 1,
-                        len(obj),
-                    )
+    #     # Create a PipeConfig object if not already a PipeConfig.
+    #     if not isinstance(config, PipeConfig):
+    #         config = PipeConfig(**Composer.to_dict(config))
+    #     pipe_fn = config.get_pipe_func()
+    #     # Return the object that is being used to execute the pipe function.
+    #     if pipe_fn is None:
+    #         logger.warning("No pipe function specified")
+    #         return obj
+    #     # Run a pipe with the pipe_fn
+    #     if config.verbose:
+    #         logger.info("Running a pipe with %s", config.pipe_target)
+    #     # Apply pipe function to each object.
+    #     if isinstance(obj, dict):
+    #         objs = {}
+    #         # Apply pipe to each object.
+    #         for no, name in enumerate(obj):
+    #             obj_ = obj[name]
 
-                objs[name] = pipe_fn(obj_, config)
-            return objs
+    #             # Apply pipe to an object.
+    #             if config.verbose:
+    #                 logger.info(
+    #                     "Applying pipe to an object [%s], %d/%d",
+    #                     name,
+    #                     no + 1,
+    #                     len(obj),
+    #                 )
 
-        return pipe_fn(obj, config)
+    #             objs[name] = pipe_fn(obj_, config)
+    #         return objs
+
+    #     return pipe_fn(obj, config)
 
     # @staticmethod
     # def get_pipelines(task: TaskConfig) -> Pipelines:
@@ -152,26 +148,7 @@ class PIPELINEs:
             task: TaskConfig to run pipelines for
             project: ProjectConfig to run pipelines
         """
-        # Set project to the project.
-        if project:
-            project.initialize()
-            task.project = project
-        # Run all pipelines in the pipeline.
-        if task.verbose:
-            logger.info("Running %s pipeline(s)", len(task.pipelines or []))
-        with elapsed_timer(format_time=True) as elapsed:
-            for pipeline in task.get_pipelines():
-                if task.verbose:
-                    logger.info("Running pipeline: %s", pipeline.name)
-                initial_object = task if pipeline.use_task_as_initial_object else None
-                PIPELINEs.run_pipeline(pipeline, initial_object, task)
-            # Print the elapsed time.
-            if task.verbose:
-                logger.info(
-                    " >> elapsed time for the task with %s pipelines: %s",
-                    len(task.pipelines or []),
-                    elapsed(),
-                )
+        task.run(project=project)
 
     @staticmethod
     def run_workflow(workflow: WorkflowConfig):
@@ -195,7 +172,7 @@ class PIPELINEs:
                 logger.info("Running task [%s] with [%s]", task_name, rc)
                 if isinstance(task, TaskConfig):
                     # Run the task if verbose is true.
-                    PIPELINEs.run_task(task, project=workflow.project)
+                    task.run(project=workflow.project)
                 elif task is not None and getattr(task, "__call__", None):
                     if rc.run_kwargs:
                         task(**rc.run_kwargs)
@@ -223,7 +200,7 @@ class PIPELINEs:
                     initial_object = (
                         task if pipeline.use_task_as_initial_object else None
                     )
-                    PIPELINEs.run_pipeline(pipeline, initial_object, task)
+                    task.run(pipeline, initial_object)
                 # Print the elapsed time.
                 if workflow.verbose:
                     logger.info(

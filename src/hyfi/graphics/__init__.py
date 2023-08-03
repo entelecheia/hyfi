fca5fa34519cb0189038e0e8b1a3988459f99710
Young Joon Lee <entelecheia@hotmail.com>
2023-08-03 09:05:55 +0900

    refactor(graphics): reorder import statements

--- src/hyfi/graphics/__init__.py
+++ src/hyfi/graphics/__init__.py
@@ -2,13 +2,13 @@ from ._collage import Collage, collage, gallery
 from .graphics import GRAPHICs
 from .motion import create_video, extract_frames, make_gif
 from .utils import (
+    find_font_file,
+    get_default_system_font,
+    get_image_font,
+    get_plot_font,
     load_image,
     load_images,
     scale_image,
-    get_image_font,
-    get_default_system_font,
-    get_plot_font,
-    find_font_file,
 )
 
 __all__ = [

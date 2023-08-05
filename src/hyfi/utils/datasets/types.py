407b73a55f94cf6393167c9bd35fe2c7e8d358f6
Young Joon Lee <entelecheia@hotmail.com>
2023-08-05 17:09:48 +0900

    feat(hyfi/utils/datasets): add types for various datasets

---
+++ src/hyfi/utils/datasets/types.py
@@ -0,0 +1,14 @@
+from typing import Union
+
+from datasets.arrow_dataset import Dataset
+from datasets.dataset_dict import DatasetDict, IterableDatasetDict
+from datasets.iterable_dataset import IterableDataset
+
+DatasetType = Union[Dataset, IterableDataset]
+DatasetDictType = Union[DatasetDict, IterableDatasetDict]
+DatasetLikeType = Union[
+    Dataset,
+    IterableDataset,
+    DatasetDict,
+    IterableDatasetDict,
+]

a98dbf07a0c4002e937cef25bffdf96fa558f859
Young Joon Lee <entelecheia@hotmail.com>
2023-08-06 19:12:04 +0900

    fix(task): use global defaults for task name and root, add properties for project directory and project workspace directory, change task root derivation logic

--- src/hyfi/path/task.py
+++ src/hyfi/path/task.py
@@ -1,17 +1,21 @@
 from pathlib import Path
 
+from hyfi.main.config import global_config
 from hyfi.utils.logging import LOGGING
 
 from .base import BasePathConfig
 
 logger = LOGGING.getLogger(__name__)
 
+__default_task_name__ = "demo-task"
+__default_task_root__ = "workspace"
+
 
 class TaskPathConfig(BasePathConfig):
     _config_name_: str = "__task__"
 
-    task_name: str = "demo-task"
-    task_root: str = "workspace"
+    task_name: str = __default_task_name__
+    task_root: str = __default_task_root__
 
     @property
     def name(self) -> str:
@@ -19,6 +23,14 @@ class TaskPathConfig(BasePathConfig):
         Returns the name of the path configuration.
         """
         return self.task_name
+
+    @property
+    def project_dir(self) -> Path:
+        return global_config.project_dir
+
+    @property
+    def project_workspace_dir(self) -> Path:
+        return global_config.project_workspace_dir
 
     @property
     def root_dir(self) -> Path:
@@ -29,7 +41,12 @@ class TaskPathConfig(BasePathConfig):
             an path to the task root directory or None if it doesn't exist or cannot be converted to a path object
         """
         # return as an path
-        path_ = Path(self.task_root)
+        if self.task_root == __default_task_root__:
+            path_ = self.project_workspace_dir
+        else:
+            path_ = Path(self.task_root)
+            if not path_.is_absolute():
+                path_ = self.project_dir / path_
         path_.mkdir(parents=True, exist_ok=True)
         return path_
 

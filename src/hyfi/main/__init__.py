770e4a8a5a0634c2643bbb0ae3d5467cd0b1eec1
Young Joon Lee <entelecheia@hotmail.com>
2023-08-06 19:10:16 +0900

    refactor(hyfi/main): update imports and globals in __init__.py

--- src/hyfi/main/__init__.py
+++ src/hyfi/main/__init__.py
@@ -1,11 +1,14 @@
-from .config import HyfiConfig
-from .config import __global_config__ as global_config
-from .config import __project_root_path__, __project_workspace_path__
+from .config import (
+    GlobalConfig,
+    __project_root_path__,
+    __project_workspace_path__,
+    global_config,
+)
 from .main import HyFI
 
 __all__ = [
     "HyFI",
-    "HyfiConfig",
+    "GlobalConfig",
     "global_config",
     "__project_root_path__",
     "__project_workspace_path__",

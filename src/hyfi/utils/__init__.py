52ff673017e8c34d76da18265ad1414c11ccba28
Young Joon Lee <entelecheia@hotmail.com>
2023-08-09 08:55:11 +0900

    fix(utils): rename simpleeval.py to safeeval.py

--- src/hyfi/utils/__init__.py
+++ src/hyfi/utils/__init__.py
@@ -7,7 +7,7 @@ from .iolibs import IOLIBs
 from .logging import LOGGING
 from .notebooks import NBs
 from .packages import PKGs
-from .simpleeval import SIMPLE_EVAL, SimpleEval
+from .safeeval import SAFEEVAL, SafeEval
 
 
 class UTILs(
@@ -20,7 +20,7 @@ class UTILs(
     LOGGING,
     NBs,
     PKGs,
-    SIMPLE_EVAL,
+    SAFEEVAL,
 ):
     def __init__(self) -> None:
         raise NotImplementedError("Use one of the static construction functions")
@@ -37,6 +37,6 @@ __all__ = [
     "NBs",
     "PKGs",
     "UTILs",
-    "SIMPLE_EVAL",
-    "SimpleEval",
+    "SAFEEVAL",
+    "SafeEval",
 ]

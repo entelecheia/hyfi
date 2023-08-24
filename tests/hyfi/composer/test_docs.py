a0f63d1ea86181b5883f5c4398a986128bc1d6bc
Young Joon Lee <entelecheia@hotmail.com>
2023-08-24 12:17:16 +0900

    feat(test_docs): add test for generating reference docs

--- tests/hyfi/composer/test_docs.py
+++ tests/hyfi/composer/test_docs.py
@@ -7,5 +7,12 @@ def test_docs_generate_configs():
     dg.generate_config_docs()
 
 
+def test_docs_generate_refs():
+    DocGenerator.generate_config()
+    dg = DocGenerator()
+    dg.generate_reference_docs()
+
+
 if __name__ == "__main__":
-    test_docs_generate_configs()
+    # test_docs_generate_configs()
+    test_docs_generate_refs()

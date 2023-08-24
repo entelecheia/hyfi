03b306b4d2dfd19b06849af8b82b81779af5e21a
Young Joon Lee <entelecheia@hotmail.com>
2023-08-24 11:23:50 +0900

    refactor(test_docs): activate generate_config in test_docs_generate_configs function

--- tests/hyfi/composer/test_docs.py
+++ tests/hyfi/composer/test_docs.py
@@ -2,9 +2,8 @@ from hyfi.composer import DocGenerator
 
 
 def test_docs_generate_configs():
-    # DocGenerator.generate_config()
+    DocGenerator.generate_config()
     dg = DocGenerator()
-    dg.generate_config()
     dg.generate_config_docs()
 
 

25b823879db2e019cf7267bdfbd0d7d4ae99ffba
Young Joon Lee <entelecheia@hotmail.com>
2023-08-24 10:48:42 +0900

    feat(tests): add tests for DocGenerator in hyfi composer

---
+++ tests/hyfi/composer/test_docs.py
@@ -0,0 +1,11 @@
+from hyfi.composer import DocGenerator
+
+
+def test_docs_generate_configs():
+    DocGenerator.generate_config()
+    dg = DocGenerator()
+    dg.generate_config_docs()
+
+
+if __name__ == "__main__":
+    test_docs_generate_configs()

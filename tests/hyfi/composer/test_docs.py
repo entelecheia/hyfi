cb5f76b56a7e9f913edf0776afeb3853dce84351
Young Joon Lee <entelecheia@hotmail.com>
2023-08-24 12:54:00 +0900

    refactor(test_docs): modify generate_docs function

--- tests/hyfi/composer/test_docs.py
+++ tests/hyfi/composer/test_docs.py
@@ -1,4 +1,16 @@
 from hyfi.composer import DocGenerator
+from typing import List, Tuple
+import subprocess
+
+
+def capture(command: List[str]) -> Tuple[bytes, bytes, int]:
+    proc = subprocess.Popen(
+        command,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    out, err = proc.communicate()
+    return out, err, proc.returncode
 
 
 def test_generate_configs():
@@ -6,12 +18,18 @@ def test_generate_configs():
     DocGenerator.generate_config()
 
 
-def test_generate_docs():
+def generate_docs():
     dg = DocGenerator()
     dg.generate_config_docs()
     dg.generate_reference_docs()
 
 
+def test_generate_docs() -> None:
+    command = ["poetry", "run", "hyfi", "+docs=__init__"]
+    out, err, exitcode = capture(command)
+    assert exitcode == 0
+
+
 if __name__ == "__main__":
     test_generate_configs()
-    test_generate_docs()
+    generate_docs()

fb1f72a9267ea580d25db0fb45476f82115a5f98
Young Joon Lee <entelecheia@hotmail.com>
2023-08-24 11:20:56 +0900

    refactor(test_docs): move generate_config method call to object instance

--- tests/hyfi/composer/test_docs.py
+++ tests/hyfi/composer/test_docs.py
@@ -2,8 +2,9 @@ from hyfi.composer import DocGenerator
 
 
 def test_docs_generate_configs():
-    DocGenerator.generate_config()
+    # DocGenerator.generate_config()
     dg = DocGenerator()
+    dg.generate_config()
     dg.generate_config_docs()
 
 
